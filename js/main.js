75317110c4c36643709685e9e7f5b8d5f9cf03bf
hxh <1246996371@qq.com>
2018-01-12 23:08:27 +0800

    'preload'

--- js/main.js
+++ js/main.js
@@ -8,21 +8,34 @@ APP = new Vue({
   },
   methods: {
     initRepo: function () {
-      let url = baseUrl + 'repo/random'
-      let _self = this
+      var url = baseUrl + 'repo/random'
+      var _self = this
+      var _item = localStorage.getItem('crepo')
+      if (!_item) {
+        axios.get(url).then(function (res) {
+          _self.repo = res.data
+          _self.preloadNext()
+        })
+      } else {
+        _self.repo = JSON.parse(_item)
+        _self.preloadNext()
+      }
+    },
+    preloadNext: function () {
+      var url = baseUrl + 'repo/random'
       axios.get(url).then(function (res) {
-        _self.repo = res.data
+        localStorage.setItem('crepo', JSON.stringify(res.data))
       })
     },
     cdn: function (name, folder, process) {
-      let url = 'https://awesomes.oss-cn-beijing.aliyuncs.com/' + folder + '/' + name
+      var url = 'https://awesomes.oss-cn-beijing.aliyuncs.com/' + folder + '/' + name
       if (process) {
         url += '?x-oss-process=style/' + process
       }
       return url
     },
     formatFresh: function (time) {
-      let days = (Date.parse(new Date()) - Date.parse(new Date(time))) / 1000 / 60 / 60 / 24
+      var days = (Date.parse(new Date()) - Date.parse(new Date(time))) / 1000 / 60 / 60 / 24
       if (days <= 20) {
         return ['often', '积极维护']
       }
